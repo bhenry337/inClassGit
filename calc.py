7b79c37298672e1c45a2fcbe34780ff0ce1163e2
bhenry337 <72040662+bhenry337@users.noreply.github.com>
2021-04-15 16:57:57 -0700

    Sum of List

--- calc.py
+++ calc.py
@@ -1,3 +1,10 @@
 def calc(a, b):
 	sum = a + b
-	print(sum)
+	#print(sum)
+	difference = a - b
+	multiply = a * b
+	divide = a / b
+
+	myList = [sum, difference, multiply, divide]
+
+	sumofList = sum + difference + multiply + divide
